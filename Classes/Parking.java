1bff78e2f3beb8d8d4229c21ccbef3b69a7f5120
Gemmy <131499327+Mohamedgamal235@users.noreply.github.com>
2024-11-12 01:47:48 +0200

    Update Parking.java

--- Classes/Parking.java
+++ Classes/Parking.java
@@ -6,7 +6,7 @@ public class ParkingLot {
     private int totalCarsServed;
 
     public ParkingLot(int totalSpots) {
-        this.parkingSpots = new Semaphore(totalSpots, true);
+        this.parkingSpots = new Semaphore(totalSpots);
         this.currentCarsInParking = 0;
         this.totalCarsServed = 0;
     }
@@ -20,19 +20,25 @@ public class ParkingLot {
                     currentCarsInParking++;
                     totalCarsServed++;
                 }
-                System.out.println("Car " + carId + " from " + gateName + " parked. (Parking Status: " + currentCarsInParking + " spots occupied");
+                System.out.println("Car " + carId + " from " + gateName + " parked. (Parking Status: " + currentCarsInParking + " spots occupied)");
 
                 Thread.sleep(parkingDuration * 1000L);
                 leaveParking(carId, gateName, parkingDuration);
             } else {
                 System.out.println("Car " + carId + " from " + gateName + " waiting for a spot.");
+                long startTime = System.currentTimeMillis();
                 parkingSpots.acquire();
+                long endTime = System.currentTimeMillis();
+                long timeTaken = endTime - startTime;
+
 
                 synchronized (this) {
                     currentCarsInParking++;
                     totalCarsServed++;
                 }
-                System.out.println("Car " + carId + " from " + gateName + " parked after waiting. (Parking Status: " + currentCarsInParking + " spots occupied)");
+
+
+                System.out.println("Car " + carId + " from " + gateName + " parked after waiting for "+ (timeTaken / 1000 )+ " units of time (Parking Status: " + currentCarsInParking + " spots occupied)");
 
                 Thread.sleep(parkingDuration * 1000L);
                 leaveParking(carId, gateName, parkingDuration);
@@ -43,6 +49,12 @@ public class ParkingLot {
     }
 
     public synchronized void leaveParking(int carId, String gateName, int parkingDuration) {
+        try{
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException e){
+            System.err.printf(e.getMessage());
+        }
         currentCarsInParking--;
         parkingSpots.release();
         System.out.println("Car " + carId + " from " + gateName + " left after "+ parkingDuration + " units of time. (Parking Status: " + currentCarsInParking + " spots occupied)");
