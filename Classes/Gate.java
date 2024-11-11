05f9de62147426c307fb029550a6618ccad065c1
ayaaasayeed <147319493+ayaaasayeed@users.noreply.github.com>
2024-11-12 00:24:43 +0200

    Update Gate.java

--- Classes/Gate.java
+++ Classes/Gate.java
@@ -1,5 +1,24 @@
 package Assignment;
 
 public class Gate implements Runnable {
+ private List<Car> cars ;
+
+    Gate(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    @Override
+    public void run(){
+        for (Car car : cars)
+            car.start();
+
+        try {
+            for (Car car : cars)
+                car.join();
+        }
+        catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
