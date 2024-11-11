c5706457d304c638e77a1e6cfd29cb3c1d9ed0eb
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-11-11 23:32:32 +0200

    Add implementation of Car.java

--- Classes/Car.java
+++ Classes/Car.java
@@ -1,4 +1,21 @@
-package Assignment;
-
 public class Car extends Thread {
+    private final int  carId ;
+    private final String gateName ;
+    private final int arrivalTime ;
+    private final int parkingDuration ;
+    private ParkingLot parkingLot ;
+
+    public Car(int carId, String gateName, int arrivalTime, int parkingDuration , ParkingLot parkingLot) {
+        this.carId = carId;
+        this.gateName = gateName;
+        this.arrivalTime = arrivalTime;
+        this.parkingDuration = parkingDuration;
+        this.parkingLot = parkingLot;
+    }
+
+
+    @Override
+    public void run() {
+        parkingLot.parkCar(carId , gateName , arrivalTime , parkingDuration);
+    }
 }
